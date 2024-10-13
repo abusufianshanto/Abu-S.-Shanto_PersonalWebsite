2af039e714f0196fd07700fef0bd90b100935efe
Abu Sufian Shanto <shantohridoy32@gmail.com>
2024-10-13 23:16:58 +0200

    Necessary Change

--- script.js
+++ script.js
@@ -3,17 +3,33 @@ function toggleMenu() {
   menu.classList.toggle("show");
 }
 
-function toggleMore(id, event) {
+function toggleMore(id) {
   const moreText = document.getElementById(id);
-  const moreBtn = event.target;
-
   if (moreText.style.display === "none") {
-    moreText.style.display = "inline";
-    moreBtn.style.display = "none"; // Hide the "More" button after click
+    moreText.style.display = "inline"; // Show the additional content
+  } else {
+    moreText.style.display = "none"; // Hide the additional content
   }
 }
-
 // Hiding my source code from the user
 document.addEventListener("contextmenu", (event) => event.preventDefault());
 // Preventing users to copy the contents
 document.addEventListener("copy", (event) => event.preventDefault());
+
+// Function to open a simple calculator in a new window
+function openCalculator() {
+  const calculatorWindow = window.open(
+    "SimpleCalculator.html",
+    "Simple Calculator",
+    "width=500,height=500"
+  );
+}
+
+function openTempConverter() {
+  const tempConverterWindow = window.open(
+    "SimpleTemperatureConverter.html",
+    "Simple Temperature Converter",
+    "width=500",
+    (height = "500")
+  );
+}
