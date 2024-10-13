a1bc26bd6eefc27cca3bf4ed660673a67b7bd85b
Abu Sufian Shanto <shantohridoy32@gmail.com>
2024-10-13 17:30:16 +0200

    So many changes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-
-      function toggleMenu() {
-        const menu = document.getElementById("menu");
-        menu.classList.toggle("show");
-      }
-
-      function toggleMore(id) {
-        const moreText = document.getElementById(id);
-        if (moreText.style.display === "none") {
-          moreText.style.display = "inline"; // Show the additional content
-        } else {
-          moreText.style.display = "none"; // Hide the additional content
-        }
-      }
-      // Hiding my source code from the user
-      document.addEventListener("contextmenu", (event) =>
-        event.preventDefault()
-      );
-      // Preventing users to copy the contents
-      document.addEventListener("copy", (event) => event.preventDefault());
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+  }
+  
+  function toggleMore(id, event) {
+    const moreText = document.getElementById(id);
+    const moreBtn = event.target;
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline";
+      moreBtn.style.display = "none"; // Hide the "More" button after click
+    }
+  }
+  
+  // Hiding my source code from the user
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+  // Preventing users to copy the contents
+  document.addEventListener("copy", (event) => event.preventDefault());
+  
