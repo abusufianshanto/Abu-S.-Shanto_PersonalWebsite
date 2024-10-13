9977b7d5d1811ff170f940335a7a4f62cf79fa3e
Abu Sufian Shanto <shantohridoy32@gmail.com>
2024-10-13 17:45:01 +0200

    Adding Projects in the website

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("show");
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+}
+
+function toggleMore(id, event) {
+  const moreText = document.getElementById(id);
+  const moreBtn = event.target;
+
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline";
+    moreBtn.style.display = "none"; // Hide the "More" button after click
   }
-  
-  function toggleMore(id, event) {
-    const moreText = document.getElementById(id);
-    const moreBtn = event.target;
-  
-    if (moreText.style.display === "none") {
-      moreText.style.display = "inline";
-      moreBtn.style.display = "none"; // Hide the "More" button after click
-    }
-  }
-  
-  // Hiding my source code from the user
-  document.addEventListener("contextmenu", (event) => event.preventDefault());
-  // Preventing users to copy the contents
-  document.addEventListener("copy", (event) => event.preventDefault());
-  
+}
+
+// Hiding my source code from the user
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+// Preventing users to copy the contents
+document.addEventListener("copy", (event) => event.preventDefault());
